57edbc4d85d298d00fc130474e15603087d80452
nguyen minh dang <146596457+thanhngucomvn999@users.noreply.github.com>
2024-05-20 17:58:59 +0800

    Add files via upload

---
+++ bufftimtiktok.py
@@ -0,0 +1,18 @@
+import pyautogui as pag
+import time
+
+time.sleep(5)
+
+print(pag.position())
+
+pag.doubleClick()
+time.sleep(1)
+pag.moveTo(545, 394)
+time.sleep(1)
+pag.click()
+time.sleep(1)
+pag.moveTO(776< 419)
+pag.doubleClick()
+
+# Point(x=776, y=419)
+# Point(x=545, y=394)
